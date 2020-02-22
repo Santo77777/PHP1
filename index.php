147b1b44f5d4a94d6894b8834d4d9eebb1145e12
Santo77777 <55029425+Santo77777@users.noreply.github.com>
2020-02-21 20:59:00 -0500

    Ejercicio 1 terminado

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php 
+$numero1 = 50;
+$numero2 = 10;
+
+echo "<strong>Taller 1</span> Generalidades de PHP</strong>";
+echo "<br>";
+echo "<br>";
+echo "<strong>Ejercicios numero 1. </strong>";
+echo "<br>";
+echo "<br>";
+echo "***********************************************************************";
+echo "<br>";
+
+echo "La suma de ".$numero1 ." + " .$numero2 ." es: ".($numero1 + $numero2);
+echo "<br>";
+echo "La resta de ".$numero1 ." - " .$numero2 ." es: ".($numero1 - $numero2);
+echo "<br>";
+echo "La multiplicación de ".$numero1 ." x " .$numero2 ." es: ".($numero1 * $numero2);
+echo "<br>";
+echo "La division de ".$numero1 ." / " .$numero2 ." es: ".($numero1 / $numero2);
+echo "<br>";
+echo "<br>";
+
+echo "<strong>Ejercicios numero 2. </strong>";
+echo "<br>";
+echo "<br>";
+echo "***********************************************************************";
+echo "<br>";
+
+$peso = 85;
+$altura = 1.78;
+$imc = $peso /($altura*$altura);
+
+switch ($variable) {
+    case 'value':
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
